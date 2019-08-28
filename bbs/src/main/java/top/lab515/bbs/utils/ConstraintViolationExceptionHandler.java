b0d2b8774166c3021946782c1dcb35dd821a14c0
seriouszyx <674965440@qq.com>
2019-08-28 10:29:47 +0800

    chore: Handle global exception.

---
+++ bbs/src/main/java/top/lab515/bbs/utils/ConstraintViolationExceptionHandler.java
@@ -0,0 +1,33 @@
+package top.lab515.bbs.utils;
+
+
+import org.thymeleaf.util.StringUtils;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ：Yixiang Zhao
+ * @date ：Created in 2019/8/27 16:13
+ * @description：
+ */
+public class ConstraintViolationExceptionHandler {
+
+    /**
+     * 获取批量异常信息
+     * @param e
+     * @return
+     */
+    public static String getMessage(ConstraintViolationException e) {
+        List<String> msgList = new ArrayList<>();
+        for (ConstraintViolation<?> constraintViolation : e.getConstraintViolations()) {
+            msgList.add(constraintViolation.getMessage());
+        }
+        String messages = StringUtils.join(msgList.toArray(), ";");
+        return messages;
+    }
+
+}
+
