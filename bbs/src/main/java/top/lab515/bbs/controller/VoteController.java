7ac3e018601f56d56d0907a779180e573b5aa2e5
seriouszyx <674965440@qq.com>
2019-09-01 20:20:45 +0800

    feat: Add vote features for blogs.

---
+++ bbs/src/main/java/top/lab515/bbs/controller/VoteController.java
@@ -0,0 +1,105 @@
+package top.lab515.bbs.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import top.lab515.bbs.domain.User;
+import top.lab515.bbs.domain.Vote;
+import top.lab515.bbs.service.BlogService;
+import top.lab515.bbs.service.VoteService;
+import top.lab515.bbs.utils.ConstraintViolationExceptionHandler;
+import top.lab515.bbs.vo.Response;
+
+import javax.validation.ConstraintViolationException;
+import java.util.Optional;
+
+/**
+ * @author ：Yixiang Zhao
+ * @date ：Created in 2019/9/1 16:44
+ * @description：点赞控制器
+ */
+@Controller
+@RequestMapping("/votes")
+public class VoteController {
+
+    @Autowired
+    private BlogService blogService;
+
+    @Autowired
+    private VoteService voteService;
+
+    /**
+     * 发表点赞
+     *
+     * @param blogId
+     * @return
+     */
+    @PostMapping
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_USER')")  // 指定角色权限才能操作方法
+    public ResponseEntity<Response> createVote(Long blogId) {
+
+        try {
+            blogService.createVote(blogId);
+        } catch (ConstraintViolationException e) {
+            return ResponseEntity.ok().body(new Response(false,
+                    ConstraintViolationExceptionHandler.getMessage(e)));
+        } catch (Exception e) {
+            return ResponseEntity.ok().body(new Response(false, e.getMessage()));
+        }
+
+        return ResponseEntity.ok().body(new Response(true, "点赞成功", null));
+    }
+
+    /**
+     * 删除点赞
+     *
+     * @return
+     */
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_USER')")  // 指定角色权限才能操作方法
+    public ResponseEntity<Response> delete(@PathVariable("id") Long id, Long blogId) {
+
+        boolean isOwner = false;
+        Optional<Vote> optionalVote = voteService.getVoteById(id);
+        User user = null;
+        if (optionalVote.isPresent()) {
+            user = optionalVote.get().getUser();
+        } else {
+            return ResponseEntity.ok().body(new Response(false, "不存在该点赞！"));
+        }
+
+        // 判断操作用户是否是点赞的所有者
+        if (SecurityContextHolder.getContext().getAuthentication() != null
+                && SecurityContextHolder.getContext().getAuthentication().isAuthenticated()
+                && !SecurityContextHolder.getContext().getAuthentication()
+                .getPrincipal().toString().equals("anonymousUser")) {
+            User principal = (User) SecurityContextHolder.getContext()
+                    .getAuthentication().getPrincipal();
+            if (principal != null && user.getUsername().equals(principal.getUsername())) {
+                isOwner = true;
+            }
+        }
+
+        if (!isOwner) {
+            return ResponseEntity.ok().body(new Response(false, "没有操作权限"));
+        }
+
+        try {
+            blogService.removeVote(blogId, id);
+            voteService.removeVote(id);
+        } catch (ConstraintViolationException e) {
+            return ResponseEntity.ok().body(new Response(false,
+                    ConstraintViolationExceptionHandler.getMessage(e)));
+        } catch (Exception e) {
+            return ResponseEntity.ok().body(new Response(false, e.getMessage()));
+        }
+
+        return ResponseEntity.ok().body(new Response(true, "取消点赞成功", null));
+    }
+}
