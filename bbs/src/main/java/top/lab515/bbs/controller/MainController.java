5c2902ef4cfd9ed0effb7f0ab01a8e1ceed337da
seriouszyx <674965440@qq.com>
2019-09-01 20:24:50 +0800

    feat: Change methods of authority management.

--- bbs/src/main/java/top/lab515/bbs/controller/MainController.java
+++ bbs/src/main/java/top/lab515/bbs/controller/MainController.java
@@ -21,13 +21,13 @@ import java.util.List;
 @Controller
 public class MainController {
 
-    private static final Long ROLE_USER_AUTHORITY_ID = 2L;
+    private static final Long ROLE_USER_AUTHORITY_ID = 2L; // 用户权限（博主）
 
     @Autowired
     private UserService userService;
 
     @Autowired
-    private AuthorityService  authorityService;
+    private AuthorityService authorityService;
 
     @GetMapping("/")
     public String root() {
@@ -36,13 +36,9 @@ public class MainController {
 
     @GetMapping("/index")
     public String index() {
-        return "index";
+        return "redirect:/blogs";
     }
 
-    /**
-     * 获取登录界面
-     * @return
-     */
     @GetMapping("/login")
     public String login() {
         return "login";
@@ -51,7 +47,7 @@ public class MainController {
     @GetMapping("/login-error")
     public String loginError(Model model) {
         model.addAttribute("loginError", true);
-        model.addAttribute("errorMsg", "登陆失败，账号或者密码错误！");
+        model.addAttribute("errorMsg", "登录失败，用户名或者密码错误！");
         return "login";
     }
 
@@ -62,21 +58,18 @@ public class MainController {
 
     /**
      * 注册用户
+     *
      * @param user
      * @return
      */
     @PostMapping("/register")
     public String registerUser(User user) {
         List<Authority> authorities = new ArrayList<>();
-        authorities.add(authorityService.getAuthorityById(ROLE_USER_AUTHORITY_ID));
+        authorities.add(authorityService.getAuthorityById(ROLE_USER_AUTHORITY_ID).get());
         user.setAuthorities(authorities);
-        userService.saveUser(user);
+        userService.registerUser(user);
         return "redirect:/login";
     }
 
-    @GetMapping("/search")
-    public String search() {
-        return "search";
-    }
 }
 
