394b7dcc9f899004d8c34cd50814bd7b496cd19e
seriouszyx <674965440@qq.com>
2019-11-29 18:21:06 +0800

    chore: Remove layui-admin.

--- bbs/mgrsite/src/main/java/com/seriouszyx/bbs/back/controller/IndexController.java
+++ bbs/mgrsite/src/main/java/com/seriouszyx/bbs/back/controller/IndexController.java
@@ -16,4 +16,5 @@ public class IndexController {
         return "home/console";
     }
 
+
 }
--- bbs/mgrsite/src/main/java/com/seriouszyx/bbs/back/controller/UserController.java
+++ bbs/mgrsite/src/main/java/com/seriouszyx/bbs/back/controller/UserController.java
@@ -1,4 +1,40 @@
 package com.seriouszyx.bbs.back.controller;
 
+import com.seriouszyx.bbs.back.util.JsonResult;
+import com.seriouszyx.bbs.base.domain.User;
+import com.seriouszyx.bbs.base.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Controller
 public class UserController {
+
+    private final int DEFAULT_PAGE_SIZE = 10;
+    private final int FIRST_PAGE_NUM = 1;
+
+    @Autowired
+    private IUserService userService;
+
+    @RequestMapping("toUserMgr")
+    public String toUserMgr() {
+        return "user/user/list";
+    }
+
+    @RequestMapping("toUserForm")
+    public String toUserForm() {
+        return "user/user/userform";
+    }
+
+    @RequestMapping("listWebUser")
+    @ResponseBody
+    public JsonResult listWebUser(Integer page, Integer limit) {
+        JsonResult jsonResult = new JsonResult(userService.listAll(page, limit).getList());
+        return jsonResult;
+    }
+
 }
